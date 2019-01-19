15a0bd660e957c89909e123795487832be7bbb75
Michael <rovbau@gmx.net>
2019-01-20 00:02:17 +0100

    Scanner mit Stepper Motor erfasst daten im Scan

---
+++ Scanner.py
@@ -0,0 +1,40 @@
+#! python
+# -*- coding: utf-8 -*-
+
+from time import sleep
+from Stepper import *
+from Lidar import *
+
+class Scanner():
+    def __init__(self):
+        self.stepper = Stepper()
+        self.lidar = Lidar()
+        self.actual_steps = 0
+        self.angle = 0
+        self.direction = "CCW"
+                
+    def do_scan(self, step = 3, min_angle = -30, max_angle = 30):        
+        while self.actual_steps < step:
+            self.actual_steps += 1     
+            dist = self.lidar.get_distance()
+            print(dist)
+
+            if self.direction == "CCW":
+                self.angle += 1
+                self.stepper.do_step(1)
+            else:
+                self.angle -= 1
+                self.stepper.do_step(-1)
+                
+            if (self.angle >= max_angle):
+                self.direction = "CW"
+            if (self.angle <= min_angle):
+                self.direction = "CCW"          
+        self.actual_steps = 0
+       
+        
+if __name__ == "__main__":
+    scanner = Scanner()
+    scanner.do_scan(step = 30)
+    print("next")
+    scanner.do_scan(step = 30)
---
+++ Stepper.py
@@ -0,0 +1,58 @@
+#! python
+# -*- coding: utf-8 -*-
+
+from time import sleep
+import RPi.GPIO as GPIO
+
+class Stepper():
+    def __init__(self):
+        GPIO.setmode(GPIO.BOARD)
+        self.PINS_OUTPUT = [7,11,13,15]
+        self.SEQ = [[1,1,0,0],
+                    [0,1,1,0],
+                    [0,0,1,1],
+                    [1,0,0,1],
+                    [1,1,0,0],
+                    [0,1,1,0],
+                    [0,0,1,1],
+                    [1,0,0,1]]
+        self.TOTAL_STEP = len(self.SEQ)
+        self.step_counter = 0
+
+        for pin in self.PINS_OUTPUT:
+            GPIO.setup(pin,GPIO.OUT)
+            GPIO.output(pin, False)
+        self.set_step_output()
+
+    def set_step_output(self):
+        for pin in range(0, 4):
+            x_pin = self.PINS_OUTPUT[pin]
+            if self.SEQ[self.step_counter][pin]!=0:
+                GPIO.output(x_pin, True)
+                
+            else:
+                GPIO.output(x_pin, False)
+                
+    def do_step(self, step, speed = 0.02):
+        for loop in range(0,abs(step)):
+            if step > 0:
+                self.step_counter += 1
+            if step < 0:
+                self.step_counter -= 1
+                
+            if (self.step_counter>=self.TOTAL_STEP):
+                self.step_counter = 0
+            if (self.step_counter<0):
+                self.step_counter = self.TOTAL_STEP - 1
+            self.set_step_output()
+            sleep(speed)
+        
+        
+if __name__ == "__main__":
+    while True:
+        stepper = Stepper()
+        stepper.do_step(90)
+        sleep (2)
+        print("minus")
+        stepper.do_step(-90)
+        sleep (2) 
